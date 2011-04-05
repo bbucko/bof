e91377aa123c9b7222489a00c7741684df6f3b94
Blazej Bucko <blazej.bucko@iogreen.pl>
2011-04-05 21:46:23 +0200

    Added fixtures in BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,21 @@
+import bof.*
+
 class BootStrap {
 
     def init = { servletContext ->
+	def lem = new Author(name: "Stanislaw", surname: "Lem")
+	lem.save()
+
+	def cyberiada = new Book(title: "Cyberiada", author: lem)
+	cyberiada.save()
+
+	def asimov = new Author(name:"Isaac", surname: "Asimov")
+	asimov.save()
+	
+	def foundation = new Book(title: "Foundation", author: asimov)
+	foundation.save()
+
+
     }
     def destroy = {
     }
